452a4828b17127055db3f52f2a2f0e37a2558676
Attaint54 <attaulbari080@gmail.com>
2026-06-15 23:26:22 +0500

    all

--- main.py
+++ main.py
@@ -1,49 +1,49 @@
-# # print("Hello")
-# # person_Name = ("Atta")
-# # person_Email = ("attafake@gmail.com")
-# # person_Password = (123321)
-# # peson_Bill = (22.99)
-# # person_Consistency = (False)
-# # print(f"How are you : {person_Name}")
-# # print(f"your email is : {person_Email}")
-# # print(f"your password is : {person_Password}")
-# # print(f"your bill is : RS.{peson_Bill}")
-# # if person_Consistency:
-# #     print("U are a consistent customer")
-# # else:
-# #     print("U are not a consistent customer")
+print("Hello")
+ person_Name = ("Atta")
+ person_Email = ("attafake@gmail.com")
+ person_Password = (123321)
+ peson_Bill = (22.99)
+ person_Consistency = (False)
+ print(f"How are you : {person_Name}")
+ print(f"your email is : {person_Email}")
+ print(f"your password is : {person_Password}")
+ print(f"your bill is : RS.{peson_Bill}")
+ if person_Consistency:
+     print("U are a consistent customer")
+ else:
+     print("U are not a consistent customer")
 
-# # name = "Jhon"
-# # phone_Number = 3202004413
-# # admin = True
-# # percentage = 78.56
+ name = "Jhon"
+ phone_Number = 3202004413
+ admin = True
+ percentage = 78.56
 
-# # print(int(percentage))    
+ print(int(percentage))    
 
  
-# # name = input("Name kya hai ladle? ")
-# # print(f"gel main aja {name} ladle")
+ name = input("Name kya hai ladle? ")
+ print(f"gel main aja {name} ladle")
 
-# # ask = input("ao gi gel main? (yes/no): ")
+ ask = input("ao gi gel main? (yes/no): ")
 
-# # if ask.lower() == "yes":
-# #     print("aja ladli")
-# # elif ask.lower() == "no":
-# #     print("nahi aa")
-# # else:
-# #     print("samajh nahi aaya, yes ya no likho.")
-
-
+ if ask.lower() == "yes":
+     print("aja ladli")
+ elif ask.lower() == "no":
+     print("nahi aa")
+ else:
+     print("samajh nahi aaya, yes ya no likho.")
 
 
 
 
 
-# lenght = float(input("Enter length"+ " "))
-# width = float(input("Enter width"+ " "))
-# unit = input("Enter unit"+ " ")
-# area_Of_Rectangle = lenght*width
-# print(f"area of rectangle is {area_Of_Rectangle}{unit}²")
+
+
+lenght = float(input("Enter length"+ " "))
+width = float(input("Enter width"+ " "))
+unit = input("Enter unit"+ " ")
+area_Of_Rectangle = lenght*width
+print(f"area of rectangle is {area_Of_Rectangle}{unit}²")
 
 
 item =input("what do u want"+" ")
