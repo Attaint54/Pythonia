1cb51ad95215e8387072778528603ec021abc9fe
Attaint54 <attaulbari080@gmail.com>
2026-06-15 22:12:35 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# print("Hello")
+# person_Name = ("Atta")
+# person_Email = ("attafake@gmail.com")
+# person_Password = (123321)
+# peson_Bill = (22.99)
+# person_Consistency = (False)
+# print(f"How are you : {person_Name}")
+# print(f"your email is : {person_Email}")
+# print(f"your password is : {person_Password}")
+# print(f"your bill is : RS.{peson_Bill}")
+# if person_Consistency:
+#     print("U are a consistent customer")
+# else:
+#     print("U are not a consistent customer")
+
+# name = "Jhon"
+# phone_Number = 3202004413
+# admin = True
+# percentage = 78.56
+
+# print(int(percentage))    
