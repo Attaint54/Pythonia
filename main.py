8144e4ba83ee49ca22ead297a7de88c03d6a7b34
Attaint54 <attaulbari080@gmail.com>
2026-06-15 23:23:21 +0500

    muliiply and divide

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-# print("Hello")
-# person_Name = ("Atta")
-# person_Email = ("attafake@gmail.com")
-# person_Password = (123321)
-# peson_Bill = (22.99)
-# person_Consistency = (False)
-# print(f"How are you : {person_Name}")
-# print(f"your email is : {person_Email}")
-# print(f"your password is : {person_Password}")
-# print(f"your bill is : RS.{peson_Bill}")
-# if person_Consistency:
-#     print("U are a consistent customer")
-# else:
-#     print("U are not a consistent customer")
+# # print("Hello")
+# # person_Name = ("Atta")
+# # person_Email = ("attafake@gmail.com")
+# # person_Password = (123321)
+# # peson_Bill = (22.99)
+# # person_Consistency = (False)
+# # print(f"How are you : {person_Name}")
+# # print(f"your email is : {person_Email}")
+# # print(f"your password is : {person_Password}")
+# # print(f"your bill is : RS.{peson_Bill}")
+# # if person_Consistency:
+# #     print("U are a consistent customer")
+# # else:
+# #     print("U are not a consistent customer")
 
-# name = "Jhon"
-# phone_Number = 3202004413
-# admin = True
-# percentage = 78.56
+# # name = "Jhon"
+# # phone_Number = 3202004413
+# # admin = True
+# # percentage = 78.56
 
-# print(int(percentage))    
+# # print(int(percentage))    
+
+ 
+# # name = input("Name kya hai ladle? ")
+# # print(f"gel main aja {name} ladle")
+
+# # ask = input("ao gi gel main? (yes/no): ")
+
+# # if ask.lower() == "yes":
+# #     print("aja ladli")
+# # elif ask.lower() == "no":
+# #     print("nahi aa")
+# # else:
+# #     print("samajh nahi aaya, yes ya no likho.")
+
+
+
+
+
+
+
+# lenght = float(input("Enter length"+ " "))
+# width = float(input("Enter width"+ " "))
+# unit = input("Enter unit"+ " ")
+# area_Of_Rectangle = lenght*width
+# print(f"area of rectangle is {area_Of_Rectangle}{unit}²")
+
+
+item =input("what do u want"+" ")
+quantity = int(input("How much do you want od it: "+" " ))
+price = float(input("what is the price:"+" "))
+total = price*quantity
+
+print(f"you habe bought {quantity}, {item}")
+print(f"total is: Rs.{total}")
